60a589cf0395af3d435449d52b871b35e896e01d
pidan1231239 <850576570@qq.com>
2017-05-25 11:08:01 +0800

    熟悉了raspicam的使用

--- OpenCVCameraDemo/OpenCVCameraDemo.cpp
+++ OpenCVCameraDemo/OpenCVCameraDemo.cpp
@@ -3,50 +3,38 @@
 #include <raspicam_cv.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "time.h"
 
 using namespace cv;
 using namespace std;
  
 int main(int argc, char **argv)
 {
-	int threshold = 100;
-	const int frameDelay = 100, maxContours = 500;
-    
 	raspicam::RaspiCam_Cv cam;
 	Mat image;
 	cam.set(CV_CAP_PROP_FORMAT, CV_8UC1);
+	cam.set(CV_CAP_PROP_FRAME_WIDTH, cam.get(CV_CAP_PROP_FRAME_WIDTH) * 0.5);
+	cam.set(CV_CAP_PROP_FRAME_HEIGHT, cam.get(CV_CAP_PROP_FRAME_HEIGHT) * 0.5);
 	if (!cam.open())
 		return 1;
     
-	const char szSourceWindow[] = "Source", szContoursWindow[] = "Contours";
+	const char szSourceWindow[] = "Camera";
 	namedWindow(szSourceWindow, WINDOW_AUTOSIZE);
-	namedWindow(szContoursWindow, WINDOW_AUTOSIZE);
-	createTrackbar("Threshold:", szSourceWindow, &threshold, 255, NULL);
-
+	double startT, endT; 
 	for (;;)
 	{
+		startT = clock();
 		RNG rng(12345);
 		cam.grab();
 		cam.retrieve(image);
-		Mat smallImage;
-		resize(image, smallImage, Size(), 0.5, 0.5);
-		imshow(szSourceWindow, smallImage);
-        
-		Mat canny_output;
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-		Canny(smallImage, canny_output, threshold, threshold * 2, 3);
-		findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-
-		Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-		for (size_t i = 0; i < std::min(contours.size(), (size_t)maxContours); i++)
-		{
-			Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-			drawContours(drawing, contours, (int)i, color, 2, 8, hierarchy, 0, Point());
-		}
-
-		imshow(szContoursWindow, drawing);
-		waitKey(frameDelay);
+		imshow(szSourceWindow, image);
+//		Mat smallImage;
+//		resize(image, smallImage, Size(), 0.1, 0.1);
+//		imshow(szSourceWindow, smallImage);
+		waitKey(100);
+		endT = clock();
+		cout << (endT - startT) / CLOCKS_PER_SEC << endl;
 	}
 	cam.release();
 }
+
